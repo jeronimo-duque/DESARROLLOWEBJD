b935f0a29e14956ab6976e9fbebaea248e09ed51
jeronimo-duque <jeronimo.duque@uao.edu.co>
2023-11-12 19:32:08 -0500

    Challenge17

---
+++ Challenge17/routes/auth.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router();
+const { check } = require('express-validator');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth.js');
+const { validarCampos } = require('../middlewares/validar-campos');
+
+router.post('/', loginUsuario)
+
+router.post(
+    '/new',
+    [
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email','El email es obligatorio').isEmail(),
+        check('password', ).isLength({min:6}),
+        validarCampos
+    ],
+    crearUsuario)
+router.get('/renew', revalidarToken )
+
+module.exports = router;
